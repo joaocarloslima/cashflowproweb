90f073c92d2e0d5182c8f2e6a126105879559abd
joaocarloslima <joaocarloslima@me.com>
2024-04-10 11:51:26 -0300

    post categoria

--- src/app/categorias/page.tsx
+++ src/app/categorias/page.tsx
@@ -1,11 +1,13 @@
 import NavBar from "@/components/NavBar"
 import { CategoriaItem } from "./CategoriaItem";
 import { Button } from "@nextui-org/button";
+import { Plus } from "lucide-react";
+import Link from "next/link";
 
 export default async function Categorias() {
 
   async function getCategorias (){
-    const resp = await fetch("http://localhost:8080/categoria")
+    const resp = await fetch("http://localhost:8080/categoria", {next:{ revalidate: 0 }})
     return await resp.json()
   }
 
@@ -18,7 +20,11 @@ export default async function Categorias() {
       <section className="flex flex-col gap-2 bg-slate-900 mt-4 p-3 rounded min-w-[500px]">
         <div className="flex justify-between">
           <h2 className="text-2xl font-bold">Categorias Cadastradas</h2>
-          <Button>nova categoria</Button>
+          <Link href="/categorias/new">
+            <Button color="primary" startContent={<Plus />}>
+              nova categoria
+            </Button>
+          </Link>
         </div>
 
         {categorias.map(categoria => <CategoriaItem categoria={categoria} /> )}
