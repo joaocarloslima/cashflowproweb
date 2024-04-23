6b21419384628526a561345c494f395beafe1c13
joaocarloslima <joaocarloslima@me.com>
2024-04-23 09:49:47 -0300

    dashboard

--- src/app/utils/icons.tsx
+++ src/app/utils/icons.tsx
@@ -1,9 +1,14 @@
-import { Apple, Book, Bus, Cigarette, Home } from "lucide-react";
+import { Apple, ArrowDown, ArrowUp, Book, Bus, Calendar, Cigarette, Home } from "lucide-react";
 
 export const icons = [
-    { name: "apple",        icon: <Apple />},
-    { name: "cigarette",    icon: <Cigarette />},
-    { name: "book",         icon: <Book />},
-    { name: "bus",          icon: <Bus />},
-    { name: "home",          icon: <Home />},
+    { name: "apple", icon: <Apple /> },
+    { name: "cigarette", icon: <Cigarette /> },
+    { name: "book", icon: <Book /> },
+    { name: "bus", icon: <Bus /> },
+    { name: "home", icon: <Home /> },
+    { name: "arrow-up", icon: <ArrowUp /> },
+    { name: "arrow-down", icon: <ArrowDown /> },
+    { name: "calendar", icon: <Calendar /> },
+
+
 ]
